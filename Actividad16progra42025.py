9caf58e362827477bb227e2d7436cc226bcc4e0e
Darwin <jackdarwinbot@gmail.com>
2025-08-13 11:07:39 -0600

    Segundo comit, se hizo las classes por aparte

--- Actividad16progra42025.py
+++ Actividad16progra42025.py
@@ -1,3 +1,57 @@
+class Libro:
+    def __init__(self, codigo, titulo, autor,anio):
+        self.codigo = codigo
+        self.titulo = titulo
+        self.autor = autor
+        self.anio=anio
+
+    def mostrar_info(self):
+        return f"Carnet: {self.codigo} - Nombre: {self.titulo} - Edad: {self.autor} - Anio: {self.anio}"
+class RegistroLibro:
+    def __init__(self):
+        self.Libros = {}
+    def agregar(self):
+        try:
+            codigoLibro = input("Ingresar el codigo del libro: ")
+            if codigoLibro in self.Libros:
+                print("Ya existe un libro con ese codigo.\n")
+                return
+
+            titulo = input("Ingresar el titulo del autor: ")
+            autor= input("Ingrese el auntor del libro")
+            anio=int("Ingrese el anio de publicacion")
+
+            self.Libros[codigoLibro] = Libro(codigoLibro, titulo, autor,anio)
+            print("Libro agregado.\n")
+        except ValueError:
+            print("Error: el anio de publicacion debe ser un número entero.\n")
+class Usuario:
+    def __init__(self, codigo, nombre,carnet,carrera):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.carrera = carrera
+
+    def mostrar_info(self):
+        return f"Carnet: {self.codigo} - Nombre: {self.nombre} - Edad: {self.carnet} - Anio: {self.carrera}"
+class RegistroEstudiantes:
+    def __init__(self):
+        self.usuarios = {}
+
+    def agregar(self):
+        try:
+            carnet = input("Ingresar carnet del estudiante: ")
+            if carnet in self.usuarios:
+                print("Ya existe un estudiante con ese carnet.\n")
+                return
+
+            nombre = input("Ingresar nombre del estudiante: ")
+            carrera = int(input("Ingresar la carrera del estudiante: "))
+
+            self.usuarios[carnet] = Usuario(carnet, nombre, carrera)
+            print("Estudiante agregado.\n")
+        except ValueError:
+            print("Error: La edad debe ser un número entero.\n")
+
 while True:
     print("Menu")
     print("1. Agregar Libros")
